5a120a168c3c0a70d1ce08c28e85fd6a993c22fd
Kosuha John <kosuha606@yandex.ru>
2020-03-07 22:20:00 +0300

    self

---
+++ exec/geegkforgeegks/string/check-if-string-is-rotated-by-two-places.php
@@ -0,0 +1,50 @@
+<?php
+
+use exer\helpers\GeeksToGeeks;
+use exer\helpers\Printer;
+
+require_once __DIR__.'/../../../vendor/autoload.php';
+
+/**
+ *
+ * Даны две строки a и b
+ * Нужно путем смешения на две позиции в любую сторону
+ * из строки b получить строку a
+ *
+ */
+
+
+Printer::ln("Check if string is rotated by two places");
+
+$main = function ($one, $two) {
+    $leftRotated = rotateStr($two, 2, 'left');
+    $rightRotated = rotateStr($two, 2, 'right');
+
+    return (int)($leftRotated===$one || $rightRotated===$one);
+};
+
+function rotateStr($str, $length, $direction = 'right') {
+    $strLen = strlen($str);
+
+    if ($direction === 'right') {
+        $right = substr($str, 0, $strLen-$length);
+        $left = substr($str, $strLen-$length, $length);
+    } elseif ($direction === 'left') {
+        $right = substr($str, 0, $length);
+        $left = substr($str, $length, $strLen-$length);
+    }
+
+    return $left.$right;
+}
+
+$input = <<<TEXT
+2
+amazon
+azonam
+geeksforgeeks
+geeksgeeksfor
+helloworld
+ldhellowor
+TEXT;
+
+echo GeeksToGeeks::string_twostr($input, $main);
--- src/helpers/GeeksToGeeks.php
+++ src/helpers/GeeksToGeeks.php
@@ -17,4 +17,17 @@ class GeeksToGeeks
 
         return $output;
     }
+
+    public static function string_twostr($input, $callback)
+    {
+        $lines = explode("\n", $input);
+        $output = '';
+        for ($i = 1; $i < count($lines); $i += 2) {
+            $one = isset($lines[$i]) ? $lines[$i] : '';
+            $two = isset($lines[$i+1]) ? $lines[$i+1] : '';
+            $output .= $callback($one, $two)."\n";
+        }
+
+        return $output;
+    }
 }
